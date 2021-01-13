a1740cfe2359a4531bd50b3ae5845fd364725581
shingirai <sechanakira@yahoo.com>
2021-01-13 18:10:27 +0200

    feat: import logic

--- src/main/kotlin/com/github/sechanakira/ecocashdataimporter/persistence/service/DataImportService.kt
+++ src/main/kotlin/com/github/sechanakira/ecocashdataimporter/persistence/service/DataImportService.kt
@@ -24,23 +24,24 @@ class DataImportService(
 
     companion object {
         const val HEADER_LINE_TO_SKIP: Long = 1
+        const val DATE_INDEX = 0
+
         const val MSISDN_INDEX = 0
-        const val DATE_INDEX = 1
-        const val COUNTRY_OF_ORIGIN_INDEX = 2
-        const val DOB_INDEX = 3
-        const val DOD_INDEX = 4
-        const val FIRST_NAME_INDEX = 5
-        const val LAST_NAME_INDEX = 6
-        const val ISSUING_COUNTRY_INDEX = 7
-        const val REGISTRAR_STATUS_INDEX = 8
-        const val STATUS_INDEX = 9
-        const val POSTAL_ADDRESS_INDEX = 10
-        const val PHYSICAL_ADDRESS_INDEX = 11
-        const val TITLE_INDEX = 12
-        const val ID_NUMBER_INDEX = 13
-        const val COMPANY_NAME_INDEX = 14
-        const val GENDER_INDEX = 15
-        const val OCCUPATION_INDEX = 16
+        const val COUNTRY_OF_ORIGIN_INDEX = 1
+        const val DOB_INDEX = 2
+        const val DOD_INDEX = 3
+        const val FIRST_NAME_INDEX = 4
+        const val LAST_NAME_INDEX = 5
+        const val ISSUING_COUNTRY_INDEX = 6
+        const val REGISTRAR_STATUS_INDEX = 7
+        const val STATUS_INDEX = 8
+        const val POSTAL_ADDRESS_INDEX = 9
+        const val PHYSICAL_ADDRESS_INDEX = 10
+        const val TITLE_INDEX = 11
+        const val ID_NUMBER_INDEX = 12
+        const val COMPANY_NAME_INDEX = 13
+        const val GENDER_INDEX = 14
+        const val OCCUPATION_INDEX = 15
     }
 
     fun importData() {
@@ -65,8 +66,17 @@ class DataImportService(
         val splitLine = line.split("|")
         val msisdn = splitLine[MSISDN_INDEX]
         val countryOfOrigin = splitLine[COUNTRY_OF_ORIGIN_INDEX]
-        val dateOfBirth = LocalDate.parse(splitLine[DOB_INDEX].split(" ")[DATE_INDEX])
-        val dateOfDeath = LocalDate.parse(splitLine[DOD_INDEX].split(" ")[DATE_INDEX])
+
+        var dateOfBirth: LocalDate? = null
+        if (!splitLine[DOB_INDEX]?.isNullOrBlank()) {
+            dateOfBirth = LocalDate.parse(splitLine[DOB_INDEX].split(" ")[DATE_INDEX])
+        }
+
+        var dateOfDeath: LocalDate? = null
+        if (!splitLine[DOD_INDEX]?.isNullOrBlank()) {
+            dateOfDeath = LocalDate.parse(splitLine[DOD_INDEX].split(" ")[DATE_INDEX])
+        }
+
         val firstName = splitLine[FIRST_NAME_INDEX]
         val lastName = splitLine[LAST_NAME_INDEX]
         val issuingCountry = splitLine[ISSUING_COUNTRY_INDEX]
