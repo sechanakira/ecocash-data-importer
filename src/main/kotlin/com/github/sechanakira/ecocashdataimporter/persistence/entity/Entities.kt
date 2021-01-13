b55f8dd69ae370acf8f80f62f0f9b04462287060
shingirai <sechanakira@yahoo.com>
2021-01-13 14:21:03 +0200

    feat: Entities

---
+++ src/main/kotlin/com/github/sechanakira/ecocashdataimporter/persistence/entity/Entities.kt
@@ -0,0 +1,36 @@
+package com.github.sechanakira.ecocashdataimporter.persistence.entity
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class CustomerData(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long,
+    val msisdn: String,
+    val countryOfOrigin: String,
+    val dateOfBirth: LocalDate,
+    val dateOfDeath: LocalDate,
+    val firstName: String,
+    val lastName: String,
+    val issuingCountry: String,
+    val registrarStatus: String,
+    val status: String,
+    val postalAddress: String,
+    val physicalAddress: String,
+    val title: String,
+    val idNumber: String,
+    val companyName: String,
+    val gender: String,
+    val occupation: String
+)
+
+@Entity
+data class ImportStatus(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime
+)
